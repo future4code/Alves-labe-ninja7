535631560242118c1f9b4790bef4e3c7bdd05e20
Ilena Acioli dos Santos <ilenaaciolids@gmail.com>
2022-06-29 12:08:29 -0300

    ajustando posições de elementos nas telas

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const FooterPagina = styled.div`
+ display: flex;
+    height: 80px;
+    border: 1px solid black;
+    justify-content: space-between;
+    position: fixed;
+    bottom:0;
+    width: 100%;
+    
+`
 
 export default class Footer extends React.Component {
   render() {
     return (
-      <div>Footer</div>
+      <FooterPagina>Footer</FooterPagina>
     )
   }
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,7 +5,10 @@ import { Button } from '@chakra-ui/react'
 
 const Teste = styled.div`
     display: flex;
+    height: 80px;
+    border: 1px solid black;
     justify-content: space-between;
+    
     
 `
 
@@ -17,7 +20,7 @@ export default class Header extends React.Component {
       <ChakraProvider>
         <Teste>
         <button>Logo</button>
-        <button onClick={()=>this.props.mudaTela("home")}>Home</button>
+        <Button colorScheme='blue' onClick={()=>this.props.mudaTela("home")}>Home</Button>
         <Button colorScheme='blue' onClick={()=>this.props.mudaTela("cadastros")}>Seja um ninja</Button>
         </Teste>
       </ChakraProvider>
